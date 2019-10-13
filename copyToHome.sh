60a503583fd1f414b41a3e9e4dbe26768d887ac8
guo122 <balanuard@163.com>
2019-10-13 22:54:40 +0800

    update

--- copyToHome.sh
+++ copyToHome.sh
@@ -4,15 +4,14 @@ cp -f tools/bash_profile.md ~/.bash_profile
 cp -f tools/bash_tools.md ~/.bash_tools
 cp -f tools/vimrc.md ~/.vimrc
 
-# todo
-sed -i '1d' ~/.bash_profile
-sed -i '1d' ~/.bash_profile
-sed -i '$d' ~/.bash_profile
+sed -i '' '1d' ~/.bash_profile
+sed -i '' '1d' ~/.bash_profile
+sed -i '' '$d' ~/.bash_profile
 
-sed -i '1d' ~/.bash_tools
-sed -i '1d' ~/.bash_tools
-sed -i '$d' ~/.bash_tools
+sed -i '' '1d' ~/.bash_tools
+sed -i '' '1d' ~/.bash_tools
+sed -i '' '$d' ~/.bash_tools
 
-sed -i '1d' ~/.vimrc
-sed -i '1d' ~/.vimrc
-sed -i '$d' ~/.vimrc
+sed -i '' '1d' ~/.vimrc
+sed -i '' '1d' ~/.vimrc
+sed -i '' '$d' ~/.vimrc
